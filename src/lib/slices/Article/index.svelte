b31e587d652bb6129dfeeb213e85e1d62503d68a
zeke <ezekiel.ravidat@gmail.com>
2024-10-03 11:12:08 +0200

    svelte basic page setup

--- src/lib/slices/Article/index.svelte
+++ src/lib/slices/Article/index.svelte
@@ -1,8 +1,26 @@
 <script>
 	/** @type {import("@prismicio/client").Content.ArticleSlice} */
 	export let slice;
+	import { PrismicImage } from "@prismicio/svelte";
+	import { PrismicRichText } from "@prismicio/svelte";
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	Placeholder component for {slice.slice_type} (variation: {slice.variation}) Slices
+<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation} class="article">
+	<div class="image">
+		<PrismicImage field={slice.primary.image}/>
+	</div>
+	<span class="title">{slice.primary.title}</span>
+	<PrismicRichText field={slice.primary.content} class="text"/>
 </section>
+
+
+<style>
+.article{
+	display: flex;
+	flex-direction: column;
+}
+
+.image{
+	width: 80%;
+}
+</style>
