9766c56dd3f5fcec60abe8288e9409ec75f25272
zeke <ezekiel.ravidat@gmail.com>
2024-10-03 11:22:07 +0200

    css fix

--- src/lib/slices/Article/index.svelte
+++ src/lib/slices/Article/index.svelte
@@ -6,9 +6,7 @@
 </script>
 
 <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation} class="article">
-	<div class="image">
-		<PrismicImage field={slice.primary.image}/>
-	</div>
+	<PrismicImage field={slice.primary.image} class="image"/>
 	<span class="title">{slice.primary.title}</span>
 	<PrismicRichText field={slice.primary.content} class="text"/>
 </section>
@@ -23,4 +21,5 @@
 .image{
 	width: 80%;
 }
+
 </style>
